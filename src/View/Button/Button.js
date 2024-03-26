e8392b887ff21d60559fb695c66053b495fcb451
A'Jahn Kimble <therealajahn@gmail.com>
2024-03-26 13:26:20 -0500

    completed the renderRythm function

---
+++ src/View/Button/Button.js
@@ -0,0 +1,30 @@
+export default function Button({bottomColor, marginTop, width}) {
+   return(
+       <div className='button' style={{display:'grid',height:'100%',width:'100%',marginTop:`${marginTop}`}}>
+           <div className='button-top'
+               style={{
+                    width:`${width ? width : '1.6vw'}`,
+                    height:'1.6vw',
+                    alignSelf:'center',
+                    justifySelf:'center',
+                    backgroundColor:'#000',
+                    borderRadius:'.25vw',
+                    border: 'solid .25vw white',
+                    zIndex:'1000',
+                }}></div>
+           <div className='button-bottom'
+               style={{
+                    position:'absolute',
+                    marginTop:'1vw',
+                    width:`${width ? width : '1.6vw'}`,
+                    height:'1.6vw',
+                    alignSelf:'center',
+                    justifySelf:'center',
+                    backgroundColor:`${bottomColor ? bottomColor : '#000'}`,
+                    borderRadius:'.25vw',
+                    border: 'solid .25vw white',
+                    zIndex:'900',
+                }}></div>
+       </div>
+   ) 
+};
--- src/View/Plate/Plate.js
+++ src/View/Plate/Plate.js
@@ -1,3 +1,6 @@
+import Button from '../Button/Button.js';
+
+
 export default function Plate({gridArea,plateColor,width,height,alignSelf,marginTop,name}) {
     function getModule(){
         switch(name){
@@ -6,11 +9,19 @@ export default function Plate({gridArea,plateColor,width,height,alignSelf,margin
             break;
         }
     }
+
     function renderRhythm(){
-        console.log('renderRhythm')
-        return(
-            // Button Frame
-            <div style={{
+        const buttonRow = {
+                display:'grid',
+                width:'100%',
+                height:'100%',
+                alignSelf:'start',
+                justifySelf:'center',
+                gridTemplateColumns: 'repeat(4,1fr)'
+        }        
+    return(
+            <div className='ButtonFrame'
+            style={{
                 display:'grid',
                 width:'95%',
                 height:'40%',
@@ -18,9 +29,44 @@ export default function Plate({gridArea,plateColor,width,height,alignSelf,margin
                 justifySelf:'center',
                 backgroundColor:'#404240',
                 borderRadius:'1vw',
-                border: 'solid .2vw white'
-            }}></div>
-        )
+                border: 'solid .2vw white',
+                gridTemplateRows:'repeat(4,1fr)',
+            }}>
+
+            <div style={buttonRow}>
+                <Button  marginTop={'-1vw'} bottomColor='#B9B9B9'/>
+                <Button  marginTop={'-1vw'} bottomColor='#B9B9B9'/>
+                <Button  marginTop={'-1vw'} bottomColor='#B9B9B9'/>
+                <Button  marginTop={'-1vw'} bottomColor='#B9B9B9'/>
+            </div>
+
+            <div style={buttonRow}>
+                <Button marginTop={'0vw'}/>
+                <Button marginTop={'0vw'}/>
+                <Button marginTop={'0vw'}/>
+                <Button marginTop={'0vw'}/>
+            </div>
+
+            <div style={buttonRow}>
+                <Button marginTop={'-.3vw'}/>
+                <Button marginTop={'-.3vw'}/>
+                <Button marginTop={'-.3vw'}/>
+                <Button marginTop={'-.3vw'}/>
+        </div>
+        
+            <div className= 'Spacebar'
+            style={{
+                display:'grid',
+                width:'100%',
+                height:'100%',
+                alignSelf:'start',
+                justifySelf:'center',
+            }}>
+                <Button width="8.5vw"/>
+            </div>
+           </div>
+    )
+
     }
     return(
         <div style={{
