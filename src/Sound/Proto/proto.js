91498e737e4ed91aba6db20e04980e9b3bd463bf
A'Jahn Kimble <therealajahn@gmail.com>
2024-05-02 14:12:42 -0500

    made kick drum and started on the hat

--- src/Sound/Proto/proto.js
+++ src/Sound/Proto/proto.js
@@ -7,6 +7,14 @@ function triggerClap(){
     makeClap();
 }
 
+function triggerKick(){
+    makeKick();
+}
+
+function triggerHat(){
+    makeHat();
+}
+
 const soundData = {
     //object with sound parameters in it 
 }
@@ -16,41 +24,65 @@ const controlData = {
     //keep track of the state of the keyboard
 }
 
+
 function makeClap(){
-    console.log('clap started')
-    //make a noise source 
+    const now = Tone.now();
+
     const noise = new Tone.Noise('brown').start();
-    //with a bit crusher on it 
-    const crusher = new Tone.BitCrusher(5);
-    //with an envelope
-    const envAttack = 0.01;
-    const envRelease = 0.1;
-    const envLength = (envAttack + envRelease) * 1000;
-    const ampEnvelope = new Tone.AmplitudeEnvelope({
-        attack:envAttack,
-        release:envRelease,
-    });
-    //connect noise to envelope
-    noise.chain(ampEnvelope,crusher,Tone.Destination);
-    ampEnvelope.triggerAttackRelease();
-    //stop noise 
-    setTimeout(() => {
-        console.log('disposal')
+    const audioEnv = new Tone.AmplitudeEnvelope({
+        attack:0.01,
+        release:0.5,
+    })
+
+    noise.chain(audioEnv,Tone.Destination);
+    audioEnv.triggerAttackRelease(now,0.001);
+
+    const envLength = audioEnv.get().attack + 
+        audioEnv.get().release;
+
+    setTimeout(()=>{
         noise.dispose();
-        crusher.dispose();
-        ampEnvelope.dispose();
-    },envLength);
-    //generate a object that has its settings
+        audioEnv.dispose();
+    },envLength * 1000);
 }
 
 function makeKick(){
+    const now = Tone.now();
     //make a sine with a pitch envelope and a audio envelope
+    const sine = new Tone.Oscillator({type:'sine'}).start();
+    const audioEnv = new Tone.AmplitudeEnvelope({
+        attack:0.01,
+        release:0.5,
+    })
+
+    sine.chain(audioEnv,Tone.Destination);
+    const pitchEnv =  new Tone.FrequencyEnvelope({
+        attack:0.01,
+        release:0.5,
+        baseFrequency:'C2',
+        octaves:4,
+    });
+    pitchEnv.connect(sine.frequency);
+    pitchEnv.triggerAttackRelease(now,0.001);
+    audioEnv.triggerAttackRelease(now,0.001);
     //put a waveshaper and distortion on top
     //generate a object that has its settings
 }
 
 function makeHat(){
+    const now = Tone.now();
     //make an fm synth with sines complex wavforms
+    const carrier = new Tone.Oscillator(220,'sine').start();
+    const modulator = new Tone.Oscillator(440,'sine').start();
+    const gain = new Tone.Gain(200);
+    modulator.chain(gain,carrier.frequency);
+//     modulator.connect(Tone.Destination);
+    carrier.connect(Tone.Destination);
+
+    //make multiply node
+    //connect gain node to modulator
+    //connect gain node to signal 
+    //connect signal to carrier frequency
     //put bit crush and short delay on top
     //generate a object that has its settings
 }
@@ -69,4 +101,13 @@ function makeSequencer(){
 
 function keyboardControl(){
     //make a keyboard that can be transposed
+    document.addEventListener('keydown',event => {
+        if(event.key === 'j'){
+            makeClap();
+        }
+        if(event.key === 'k'){
+            makeKick();
+        }
+    })
 }
+keyboardControl();
