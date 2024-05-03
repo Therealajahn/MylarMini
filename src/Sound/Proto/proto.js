b99be6fc8ac1a36aa6e6c92c91989dcbaf260b1f
A'Jahn Kimble <therealajahn@gmail.com>
2024-05-03 09:20:46 -0500

    barely solved the kick drum but I have to run the entire kickInit function every time I want to trigger it

--- src/Sound/Proto/proto.js
+++ src/Sound/Proto/proto.js
@@ -3,21 +3,24 @@ async function startAudio(){
     console.log('audio started')
 }
 const clap = clapInit();
+const kick = kickInit();
+const hat = hatInit();
+const bass = bassInit();
 
 function triggerClap(){
     clap();
 }
 
 function triggerKick(){
-    makeKick();
+    kickInit();
 }
 
 function triggerHat(){
-    makeHat();
+    hat();
 }
 
 function triggerBass(){
-    makeBass();
+    bass();
 }
 
 const soundData = {
@@ -35,15 +38,17 @@ function clapInit(){
     const noise = new Tone.Noise('white').start();
     const audioEnv = new Tone.AmplitudeEnvelope({
         attack:0.01,
-        release:0.5,
+        release:1,
     })
 
     const hiPass = new Tone.Filter(900,'highpass');
     const loPass = new Tone.Filter(2000,'lowpass');
-    const filterLFO = new Tone.Oscillator(0.1,'sawtooth').start();
+    const filterLFO = new Tone.Oscillator(80,'sawtooth').start();
+    const filterLFOGain = new Tone.Gain(300);
     const crusher = new Tone.BitCrusher(8);
 
-    filterLFO.fan(hiPass.frequency,loPass.frequency)
+    filterLFO.connect(filterLFOGain);
+    filterLFOGain.fan(hiPass.frequency,loPass.frequency)
     noise.chain(hiPass,loPass,audioEnv,crusher,Tone.Destination);
 
     const envLength = audioEnv.get().attack + 
@@ -57,63 +62,41 @@ function clapInit(){
 //                 noise.dispose();
 //                 audioEnv.dispose();
 //             },envLength * 1000);
-        }
+    }
 }
 
-function makeKick(){
-    const now = Tone.now();
-    //SOUND GENERATION
-    const sine = new Tone.Oscillator('c0','sine').start();
+function kickInit(){
+//SOUND GENERATION
+    const sine = new Tone.Oscillator('c1','sine').start();
     const noise = new Tone.Noise('brown').start();
-    const noiseGain = new Tone.Gain(0.05)
-
+    const noiseGain = new Tone.Gain(0.1);
+    
 //SOUND SHAPING
     const audioEnv = new Tone.AmplitudeEnvelope({
         attack:0.01,
-        decay:0.2,
-        sustain:1,
-        release:0.5,
-    })
-    const pitchEnv =  new Tone.FrequencyEnvelope({
-        attack:0.05,
-        attackCurve:'exponential',
-        release:0.05,
-        releaseCurve:'exponential',
-        baseFrequency:'c1',
-        octaves:4,
+        sustain:0.5,
+        release:1,
     });
+
+    sine.chain(audioEnv,Tone.Destination);
+    noise.chain(noiseGain,audioEnv,Tone.Destination);
+
+
+    const pitchEnv = new Tone.FrequencyEnvelope({
+        attack:0.001,
+        release:0.001,
+        baseFrequency:'c2',
+        octaves:1,
+    })
+
     pitchEnv.connect(sine.frequency);
-    const pitchLFO = new Tone.LFO(0.2,5,5).start();
-    pitchLFO.connect(sine.frequency);
-   
-//EFFECTS
-    const shaper = new Tone.Chebyshev(1);
-    const distortion = new Tone.Distortion(0.2);
 
-//OUTPUT
-    audioEnv.chain(distortion,shaper,Tone.Destination);
-    noise.chain(noiseGain,audioEnv);
-    sine.chain(audioEnv);
-
-//TRIGGERS
-    pitchEnv.triggerAttackRelease(now,0.001);
-    audioEnv.triggerAttackRelease(now,0.02);
-
-    const envLength = audioEnv.get().attack + 
-        audioEnv.get().release + 0.001;
-
-    setTimeout(()=>{
-        sine.dispose();
-        noise.dispose();
-        audioEnv.dispose();
-        pitchEnv.dispose();
-        shaper.dispose();
-        distortion.dispose();
-    },envLength * 1000);
+    const now = Tone.now();
+    audioEnv.triggerAttackRelease(now,0.001)
+    pitchEnv.triggerAttackRelease(now,0.001)
 }
 
-function makeHat(){
-    const now = Tone.now();
+function hatInit(){
     //make an fm synth with sines complex wavforms
     const carrier = new Tone.Oscillator('a1','sine').start();
     const modulator = new Tone.Oscillator('a#1','sine').start();
@@ -125,7 +108,11 @@ function makeHat(){
         release:1,
     })
     carrier.chain(audioEnv,Tone.Destination);
-    audioEnv.triggerAttackRelease(now,'0.001');
+
+    return () => {
+        const now = Tone.now();
+        audioEnv.triggerAttackRelease(now,'0.001');
+    }
     //make multiply node
     //connect gain node to modulator
     //connect gain node to signal 
@@ -134,8 +121,7 @@ function makeHat(){
     //generate a object that has its settings
 }
 
-function makeBass(){
-    const now = Tone.now();
+function bassInit(){
     //make a vibrato sine wave bass 
     const wave = new Tone.Oscillator('c1','sine').start();
     const audioEnv = new Tone.AmplitudeEnvelope({
@@ -143,7 +129,11 @@ function makeBass(){
         release: 1,
     });
     wave.chain(audioEnv,Tone.Destination);
-    audioEnv.triggerAttackRelease(now,'0.001');
+
+    return () => {
+        const now = Tone.now();
+        audioEnv.triggerAttackRelease(now,'0.001');
+    }
     //put filter, waveshaper, distortion, and short reverb over it
     //generate a object that has its settings
 }
@@ -161,13 +151,13 @@ function keyboardControl(){
             clap();
         }
         if(event.key === 'k'){
-            makeKick();
+            kickInit();
         }
         if(event.key === 'l'){
-            makeHat();
+            hat();
         }
         if(event.key === ';'){
-            makeBass();
+            bass();
         }
     })
 }
